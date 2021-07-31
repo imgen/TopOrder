0254971ca0ac0d84882e6ec5e2b4186e5cfce176
billshu <bill.shu@gravitysupplychain.com>
2021-07-31 14:34:04 +0800

    Add benchmark package

---
+++ src/TopSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopOrder
+{
+    public class TopSelector
+    {
+        public string SelectKthElement(string[] elements, int k = 0, StringComparer comparer = null)
+        {
+            k = k <= 0 ? 10 : k;
+            comparer ??= StringComparer.InvariantCultureIgnoreCase;
+
+        }
+    }
+}
